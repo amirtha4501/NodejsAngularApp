9448acbccd0aeccb447cd257f95eeb60dd58f4ac
Amirtha <amirtha4501@gmail.com>
2020-04-15 15:16:36 +0530

    Pipes

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-// Write Streams
 var fs = require('fs');
 
-var writeData = "Hello World";
-var writableStream = fs.createWriteStream('output.txt');
-writableStream.write(writeData, 'UTF8');
-writableStream.end();
-writableStream.on('finish', function() {
-	console.log("Write completed");
-})
+var readableStream = fs.createReadStream('in.txt');
+var writableStream = fs.createWriteStream('out.txt');
+
+readableStream.pipe(writableStream);
