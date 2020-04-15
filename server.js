c96a1858be2f7c58ff12abf755a0c85d76c53cea
Amirtha <amirtha4501@gmail.com>
2020-04-15 16:37:57 +0530

    Routers

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 var http = require('http');
+var url = require('url');
 
-function startServer() {   
+function startServer(route) {   
     function onRequest (request, response) {
-        console.log("Request received");
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request received for " + pathname);
+        route(pathname);
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write("Hello from our server module");
         response.end();
     }
-    http.createServer(onRequest).listen(8888);
-    
+    http.createServer(onRequest).listen(8888);  
     console.log("Server started on localhost port 8888");
 }
 
